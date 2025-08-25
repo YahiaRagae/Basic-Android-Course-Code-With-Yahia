ceb0ceb5ee6e2fc67061b6fec446ab8a234b7132
Yahia Ragae <yahia@mrxprt.com>
2025-08-26 07:30:54 +1200

    video 3

--- app/src/main/java/com/goodapps/basicui/MainActivity.kt
+++ app/src/main/java/com/goodapps/basicui/MainActivity.kt
@@ -17,10 +17,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -29,6 +35,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.goodapps.basicui.ui.theme.BasicUITheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +52,7 @@ class MainActivity : ComponentActivity() {
                             .padding(paddingValues = innerPadding)
                             .fillMaxSize()
                     ) {
-                        GetAllPosts()
+                        GetContent()
                     }
                 }
             }
@@ -50,60 +60,72 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun GetAllPosts(){
-        Column(
-            modifier = Modifier.verticalScroll(rememberScrollState())
-        ) {
-            GetPost()
+    fun GetContent(){
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            var number1 by remember { mutableStateOf("") }
+            var number2 by remember { mutableStateOf("") }
+            var sum by remember { mutableStateOf("") }
+
+            TextField(
+                value = number1,
+                onValueChange = { newValue->
+                    number1 = newValue
+                },
+                placeholder = {
+                    Text("Enter first number")
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
             Spacer(modifier = Modifier.size(8.dp))
-            GetPost()
+            TextField(
+                value = number2,
+                onValueChange = { newValue->
+                    number2 = newValue
+                },
+                placeholder = {
+                    Text("Enter second number")
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
             Spacer(modifier = Modifier.size(8.dp))
-            GetPost()
+            Text(
+                text = sum,
+                modifier = Modifier.padding(8.dp)
+            )
             Spacer(modifier = Modifier.size(8.dp))
-            GetPost()
-            Spacer(modifier = Modifier.size(8.dp))
-            GetPost()
-        }
-    }
-    @Composable
-    fun GetPost(){
-        Column(
-            modifier = Modifier
-                .padding(all = 8.dp)
-        ) {
+            HorizontalDivider()
             Row {
-                Image(
-                    painter = painterResource(R.drawable.icon),
-                    contentDescription = "صورة"
-                )
-                Column(
-                    modifier = Modifier.padding(start = 8.dp)
-                ) {
-                    Text(
-                        text = "Tamer Mahmoudِ",
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Blue
-                    )
-                    Spacer(modifier = Modifier.size(5.dp))
-                    Text(text = "5m")
+                Button(
+                    modifier = Modifier.weight(1F),
+                    onClick = {
+                    val number1Int = number1.toInt()
+                    val number2Int = number2.toInt()
+
+                    sum = (number1Int + number2Int).toString()
+                }) {
+                    Text("Sum")
+                }
+                Spacer(modifier = Modifier.size(10.dp))
+                Button(
+                    modifier = Modifier.weight(1F),
+                    onClick = {
+                    number1 = ""
+                    number2 = ""
+                    sum = ""
+                }) {
+                    Text("Clear")
                 }
             }
-            Spacer(modifier = Modifier.size(8.dp))
-            Image(
-                modifier = Modifier
-                    .background(Color.Red)
-                    .fillMaxWidth(),
-                painter = painterResource(R.drawable.main_image),
-                contentDescription = "",
-                contentScale = ContentScale.Inside
-            )
         }
     }
+
 
     @Preview(showBackground = true)
     @Composable
     fun ScreenPreview(){
-        GetAllPosts()
+        GetContent()
     }
 }
 
